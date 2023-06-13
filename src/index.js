212fdc333367314d799e2d943cf435ed76c7b46e
+ <jkunins95@gmail.com>
2023-06-13 14:48:04 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskTxt = e.target["new-task-description"].value
+
+    const list = document.querySelector('#tasks')
+    const newLi = document.createElement('li')
+    newLi.textContent = taskTxt
+
+    list.appendChild(newLi)
+  })
 });
